00f7658761bf0ae3a7233bbaa1366061104d4205
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 00:56:41 +0100

    solution to task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,11 @@
  * section header: Section description)*
  * Return:0 Description of the returned value
  */
-int main(void) {
+int main(void){
 char c;
 for (c = 'a'; c <= 'z'; ++c)
          putchar(c);
+         putchar(\n);
 return (0);
 }
 
