45a5ba03ccb1b99501f96da62e319f2248d34b8e
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 00:15:29 +0100

    solution to task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+        int ch;     
+    
+             /* print lowercase letters */
+
+             for (ch= ‘a’; ch<= ‘z’; ch++)
+{
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+                  getch();
+}
+return (0);
+}
