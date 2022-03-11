2a5d6acfb30fec0dc2d3df2a8749890983958943
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-11 22:46:51 +0100

    updated solution

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,17 +10,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-if(n>0)
-{
-printf("%n is positive");
-}
-else if(n==0)
-{
-printf("%n is zero");
-}
-else if(n<0)
-{
-printf("%n is negative");
-}
+printf ("Last digit of %n is ")
 	return (0);
 }
