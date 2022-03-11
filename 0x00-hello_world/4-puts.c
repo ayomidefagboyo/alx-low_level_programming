77ff41260f04729319bb52a770017c3a37837972
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-11 02:37:26 +0100

    added solution for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /* main cointaionexcucution statement */
-int main (void)
-{
-    puts("\"programming is like building a multilingual puzzle");
-return(0);
+int main(void)
+{ puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
