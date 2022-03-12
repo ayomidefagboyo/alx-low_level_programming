7beb6737a5849cab5eabe55a29566e4032d8a046
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 05:36:43 +0100

    test

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - funtion to all possible different combinations of two digits
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+int x = 0;
+int y =1;
+for (x = 48; x < 58; x++)
+{
+putchar(x);
+return (0);
+}
+
+
