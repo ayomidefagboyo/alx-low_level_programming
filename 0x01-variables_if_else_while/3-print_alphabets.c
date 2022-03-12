071a8ae26b7b741232ab3779f00b2e47834ae799
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 01:48:23 +0100

    solution to task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - funtion to generate alphabets
+ * main - prints out the alphabets in lower case and in upper case too
  * section header: Section description)*
  * Return:0 Description of the returned value
  */
@@ -12,7 +12,7 @@ for (c = 'a'; c <= 'z'; ++c)
 putchar(c);
 for (c = 'A'; c <= 'Z'; ++c)
 putchar(c);
-
+putchar('\n');
 return (0);
 }
 
