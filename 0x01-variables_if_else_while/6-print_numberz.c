1917a0f2a641b12d8658484e97cc9a59f7e254ad
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 02:39:52 +0100

    solution to task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - funtion to generate alphabets
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 10; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
+
