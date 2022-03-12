8f9af784f2d2f484c733f0003a88c374feaaba20
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 01:05:48 +0100

    solution to task 3

---
+++ 3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - funtion to generate alphabets
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+char c;
+for (c = 'A'; c <= 'Z'; ++c)
+putchar(c);
+return (0);
+}
+
