1fac99c7ef213a8c744978ca38da9368dd9c47fd
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 03:38:29 +0100

    solution to task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - funtion that prints all possible combinations of single-digit numbers.
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 9; i++)
+putchar(i);
+putchar(',')
+putchar(j);
+putchar('\n');
+return (0);
+}
+
