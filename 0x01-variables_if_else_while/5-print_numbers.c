bfe6bc66a6f60bdc95f762fbb4465f63325fdeb3
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 02:32:13 +0100

    solution totask 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - funtion to print all single digits number
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+int i;
+for(i=0, i<10, i++)
+printf("%n",i);
+return (0);
+}
+
+
