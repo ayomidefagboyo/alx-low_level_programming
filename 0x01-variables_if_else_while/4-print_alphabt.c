00f17f81411e31b9ee936c292a21e5b4ee075829
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 01:58:15 +0100

    solution to task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+/**
+ * main - prints alphabets in lowercase
+ * section header: Section description)*
+ * Return:0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+
+if (c != 'e' || c != 'q' )
+{
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
+
