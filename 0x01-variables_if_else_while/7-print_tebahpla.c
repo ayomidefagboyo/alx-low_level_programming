b49e16fbeadc5e813b6ca57d6cbd4feb4815c3c2
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-12 03:18:41 +0100

    solution to task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - funtion to generate alphabets in reverse
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
