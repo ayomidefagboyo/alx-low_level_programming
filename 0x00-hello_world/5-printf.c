70262c1a3efdb461e71c883215631ce266c6d683
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-13 18:57:38 +0100

    added changes to code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - funtion that prints combinations of single-digit numbers.
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art\n")
+return (0);
+}
+
