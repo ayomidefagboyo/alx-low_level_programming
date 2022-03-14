32e01f754d11d6a0f3a23280fc648c3b43a68c2f
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-14 13:21:19 +0100

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*
-* main - cointaion excucution statement 
-*/
+/**
+ * main - ustsing puts function.
+ * section header: Section description)*
+ * Return:0 Description of the returned value
+ */
 int main(void)
 { 
 puts("\"Programming is like building a multilingual puzzle");
