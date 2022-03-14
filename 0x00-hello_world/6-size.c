0c89d4cd39f15fd7a2ad022e169415ea597c3aa6
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-14 14:33:27 +0100

    solution update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,22 +4,20 @@
  * section header: Section description)*
  * Return:0 Description of the returned value
  */
-int main() {
-    int intType;
-    float floatType;
-    long  longType;
-    long long lType;
-    char charType;
+int main(void)
+{
+int intType;
+float floatType;
+long  longType;
+long long lType;
+char charType;
 	
-
-    // sizeof evaluates the size of a variable
+    /* sizeof evaluates the size of a variable*/
     printf("Size of a char: %zu byte(s)\n", sizeof(charType));
     printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
     printf("Size of a long int: %zu bytes(s)\n", sizeof(longType));
     printf("Size of a long long int: %zu bytes(s)\n", sizeof(lType));
     printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
-    
-
 return (0);
 }
 
