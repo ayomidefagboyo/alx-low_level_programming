901540385f9c2fcb5e5d9de5f354f4630e19d745
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-15 17:26:57 +0100

    solution to task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - funtion that prints putchar
+(* a blank line
+ * Description: Longer description of the function)
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
+{
+char ch ;
+for (ch = 'a';ch <= 'z';ch++)
+putchar(ch);
+putchar('/n');
+return(0);
+}
