afb1e9f43ef479f7a245cfde1951bc8fe2bf7ae4
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-21 10:35:23 +0100

    initial commit

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory contains my solution to pointers and array tasks 
---
+++ practice/putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+printf ("hello %s,you have saved N%d in the last %d weeks\n","ayo",4000,3);
+return(0);
+}
