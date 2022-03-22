82e49232b736cbce697e0f34ecfe8562acb5bd87
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-22 01:46:44 +0100

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,16 @@
 /**
  * _strlen - function that returns the length of a string
  * @s: char pointer type
+ * Return: length of string
  */
 
 
 int _strlen(char *s)
 {
+int c;
 
+	for (c = 0; s[c] != '\0'; c++)
+		;
 
-return (*s);
+return (c);
 }
