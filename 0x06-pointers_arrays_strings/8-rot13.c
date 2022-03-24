ef1602c4ab70abf0a345cf9840645a0a0f4d9a90
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-03-25 00:48:32 +0100

    solution to 8update

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,24 +1,26 @@
-#include "main.h"
-
+include "main.h"
 /**
- * rot13 - rotate characters 13 places in the alphabet
- * @s: string
- * Return: string `s` rotated
+ * rot13 - encode string  you rot 13 so move 13 spaces.
+ * @str: string to be encoded
+ *
+ * Return: encoded string.
  */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		for (j = 0; j < 52; ++j)
 		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
+			if (str[i] == arr1[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
 		}
 	}
-	return (s);
+	return (str);
 }
