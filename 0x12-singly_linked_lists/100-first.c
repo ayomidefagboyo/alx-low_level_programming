45d78d0914c5e5ce480830b30b3789f198153bc8
ayomide fagboyo <ayomide_fagboyo@yahoo.com>
2022-04-22 02:02:36 +0100

    100 first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
-k#include "lists.h"
+#include <stdio.h>
+
+void tortoiseAndHare(void) __attribute__ ((constructor));
 
 /**
- * quick_p - print before main function.
- * we use __attribute__((constructor)) in the header to do this.
- * Return: nothing.
- */
-void quick_p(void)
+ * tortoiseAndHare - print line before main
+ * Return: nothing
+ **/
+void tortoiseAndHare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
